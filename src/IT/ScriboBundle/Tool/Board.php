1872ca3a0a50b63d306ac3030f019d02fb53b9a6
Javier Herran <ejherran.c@gmail.com>
2014-05-21 05:14:27 -0500

    Facture Mode

--- src/IT/ScriboBundle/Tool/Board.php
+++ src/IT/ScriboBundle/Tool/Board.php
@@ -19,7 +19,7 @@ class Board
             
             if($con)
             {    
-                $r = mysql_query("select orden.id as orden, orden.status as estado, (select action from proceso where proceso.orden_id=orden.id order by proceso.id desc limit 1 ) as pro, cliente.name as cliente, orden.date as inicio, orden.time * 3600 as tiempo, TIME_TO_SEC(TIMEDIFF(now(), orden.date)) as lapso from orden, cliente where orden.status<>'X' and orden.cliente_id=cliente.id order by inicio asc;", $con);
+                $r = mysql_query("select orden.id as orden, orden.status as estado, (select action from proceso where proceso.orden_id=orden.id order by proceso.id desc limit 1 ) as pro, cliente.name as cliente, orden.date as inicio, orden.time * 3600 as tiempo, TIME_TO_SEC(TIMEDIFF(now(), orden.date)) as lapso from orden, cliente where orden.status<>'X' and orden.mode<>'U' and orden.cliente_id=cliente.id order by inicio asc;", $con);
                 if($r)
                 {
                     while($row = mysql_fetch_assoc($r))
@@ -50,7 +50,7 @@ class Board
             
             if($con)
             {    
-                $r = mysql_query("select orden.id as orden, orden.status as estado, (select action from proceso where proceso.orden_id=orden.id order by proceso.id desc limit 1 ) as pro, (select concat(personal.surname, ' ', personal.name) from proceso, usuario, personal where proceso.orden_id=orden.id and usuario.id=proceso.recibe_id and personal.id=usuario.personal_id and proceso.status='O' order by proceso.id desc limit 1) as recibe, orden.date as inicio, orden.time * 3600 as tiempo, TIME_TO_SEC(TIMEDIFF(now(), orden.date)) as lapso from orden where orden.status<>'X' order by inicio asc;", $con);
+                $r = mysql_query("select orden.id as orden, orden.status as estado, (select action from proceso where proceso.orden_id=orden.id order by proceso.id desc limit 1 ) as pro, (select concat(personal.surname, ' ', personal.name) from proceso, usuario, personal where proceso.orden_id=orden.id and usuario.id=proceso.recibe_id and personal.id=usuario.personal_id and proceso.status='O' order by proceso.id desc limit 1) as recibe, orden.date as inicio, orden.time * 3600 as tiempo, TIME_TO_SEC(TIMEDIFF(now(), orden.date)) as lapso from orden where orden.status<>'X' and orden.mode<>'U' order by inicio asc;", $con);
                 if($r)
                 {
                     while($row = mysql_fetch_assoc($r))
